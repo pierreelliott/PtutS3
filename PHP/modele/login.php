f10d547e07e016252ff8c30334c1f94201e29769
Axel <axel.bertrand2@etu.univ-lyon1.fr>
2016-11-30 19:37:13 +0100

    changement des paramètres de connexion
    Les fichier PHP sont upload sur le site mais la connexion est refusée

--- PHP/modele/login.php
+++ PHP/modele/login.php
@@ -1,22 +1,5 @@
 <?php
-//    $serveur = "phpmyadmin.alwaysdata.com";
-//    $utilisateur = "sushinoss";
-//    $mdp = "sushinos";
-//    $base = "sushinoss_bd";
-    
-	if($_SERVER["SERVER_NAME"] == "localhost")
-	{
-		$serveur = "localhost";
-		$utilisateur = "root";
-		$mdp = "";
-		$base = "ptuts3";
-	}
-	else if($_SERVER["SERVER_NAME"] == "iutdoua-web.univ-lyon1.fr")
-	{
-		$serveur = "localhost";
-		$utilisateur = "p1503940";
-		$mdp = "241638";
-		$base = "p1503940";
-	}
-    
-
+    $serveur = "phpmyadmin.alwaysdata.com";
+    $utilisateur = "sushinoss";
+    $mdp = "sushinos";
+    $base = "sushinoss_bd";
