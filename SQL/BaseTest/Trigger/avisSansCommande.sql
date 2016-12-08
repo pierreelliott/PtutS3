e4778c3bf5bc6d346e4fe494727565c9a82a2547
BROUTIER THOMAS <thomas.broutier@gmail.com>
2016-12-08 17:46:57 +0100

    Modification des triggers
    Triggers portant sur la table supprimée CommandeEnregistree

--- SQL/BaseTest/Trigger/avisSansCommande.sql
+++ SQL/BaseTest/Trigger/avisSansCommande.sql
@@ -1,9 +1,9 @@
 CREATE TRIGGER `avisSansCommande` BEFORE INSERT ON `avis`
-FOR EACH ROW Begin 
+FOR EACH ROW Begin
 
 DECLARE v_nbCommande int;
 DECLARE NO_AVIS CONDITION FOR SQLSTATE '12000';
-select count(numcommande) into v_nbCommande from commandeEnregistree where new.numuser = numuser;
+select count(numcommande) into v_nbCommande from commande where new.NumUser = NumUser;
 
 if v_nbCommande = 0 THEN
 	SIGNAL NO_AVIS
@@ -14,5 +14,5 @@ end if;
 End;
 
 /* Retourne une erreur sur alwaysdata.com
-=>  #1235 - This version of MariaDB doesn't yet support 
+=>  #1235 - This version of MariaDB doesn't yet support
 'multiple triggers with the same action time and event for one table' */
