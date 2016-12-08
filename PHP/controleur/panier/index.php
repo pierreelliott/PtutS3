07917093b75cfa02f74820c84983b6091ab73743
Axel <axel.bertrand2@etu.univ-lyon1.fr>
2016-12-08 15:51:20 +0100

    Implémentation suppression du panier

--- PHP/controleur/panier/index.php
+++ PHP/controleur/panier/index.php
@@ -3,60 +3,50 @@
     
     session_start();
     
+    // On crée le panier s'il n'existe pas déjà
     if(!isset($_SESSION["panier"]))
     {
         $_SESSION["panier"] = array();
-        $_SESSION["panier"]["libelle"] = array();
-        $_SESSION["panier"]["description"] = array();
-        $_SESSION["panier"]["sourceMoyen"] = array();
-        $_SESSION["panier"]["quantite"] = array();
-        $_SESSION["panier"]["prix"] = array();
     }
     
     if(isset($_GET["action"]) && isset($_GET["produit"]))
     {
         $tabParams = explode(',', $_GET["produit"]);
-        $positionProduit = array_search($tabParams[1],  $_SESSION["panier"]["libelle"]);
-        
-        switch ($_GET["action"])
+        if(count($tabParams) != 5 and count($tabParams) != 6)
         {
-            case "ajout":
-                if($positionProduit !== false)
-                {
-                    $_SESSION["panier"]["quantite"][$positionProduit] += 1;
-                }
-                else
-                {
-                    array_push($_SESSION["panier"]["libelle"], $tabParams[1]);
-                    array_push($_SESSION["panier"]["description"], $tabParams[2]);
-                    array_push($_SESSION["panier"]["sourceMoyen"], $tabParams[3]);
-                    array_push($_SESSION["panier"]["quantite"], 1);
-                    array_push($_SESSION["panier"]["prix"], $tabParams[4]);
-                } 
-            break;
-            case "suppression":
-                $tmp = array();
-                $tmp["libelle"] = array();
-                $tmp["quantite"] = array();
-                $tmp["prix"] = array();
+            echo "Le produit n'est pas reconnu";
+        }
+        else
+        {
+            switch ($_GET["action"])
+            {
+                case "ajout":
 
-                for($i = 0; $i < count($_SESSION["panier"]["libelle"]); $i++)
-                {
-                   if ($_SESSION["panier"]["libelle"][$i] !== $tabParams[1])
-                   {
-                      array_push( $tmp["libelle"],$_SESSION["panier"]["libelle"][$i]);
-                      array_push( $tmp["quantite"],$_SESSION["panier"]["quantite"][$i]);
-                      array_push( $tmp["prix"],$_SESSION["panier"]["prix"][$i]);
-                   }
-                }
-                //On remplace le panier en session par notre panier temporaire à jour
-                $_SESSION["panier"] =  $tmp;
-                //On efface notre panier temporaire
-                unset($tmp);
-            break;
-            default : echo 'L\'action demandée n\'est pas reconnue';
-        }      
+                    if(isset($_SESSION["panier"][$tabParams[0]]))
+                    {
+                        $_SESSION["panier"][$tabParams[0]]["quantite"] += 1;
+                    }
+                    else
+                    {
+                        $tmp = array(
+                            "libelle" => $tabParams[1],
+                            "description" => $tabParams[2],
+                            "source" => $tabParams[3],
+                            "quantite" => 1,
+                            "prix" => $tabParams[4]
+                        );
+
+                        $_SESSION["panier"][$tabParams[0]] = $tmp;
+                    }
+                    
+                    header('Location: carte.php');
+                break;
+                case "suppression":
+                    unset($_SESSION["panier"][$tabParams[0]]);
+                break;
+                default : echo 'L\'action demandée n\'est pas reconnue';
+            }      
+        }    
     }
     
     include_once('vue/panier/index.php');
-
