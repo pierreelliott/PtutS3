ae9768b9ca5d7eee1246a03d2c072a5986e3bd47
Axel <axel.bertrand2@etu.univ-lyon1.fr>
2016-12-30 15:39:31 +0100

    Correction de quelques erreurs
    J'ai aussi changé l'indentation pour que ça s'ffiche mieux sur netbeans
    (concrètement on s'en fout mais voilà) du le fichier de projet netbeans
    a été ajouté

--- reformatage/controleur/controleurPrincipal.php
+++ reformatage/controleur/controleurPrincipal.php
@@ -1,52 +1,52 @@
 <?php
-	session_start();
-	/* Son but rediriger en fonction de la page où l'on se trouve vers le bon controleur */
+    session_start();
+    /* Son but rediriger en fonction de la page où l'on se trouve vers le bon controleur */
 
-	
-	# On crée un formulaire invisible pour demander l'appel d'une page
-	
-	/*if(isset($_POST["page"])) $page = $_POST["page"];
-	else $page = "accueil";*/
-	
-	if(isset($_GET["page"])) $page = $_GET["page"];
-	else $page = "accueil";
-	
-	switch($page)
-	{
-		case "carte":
-			include_once("carteControleur.php");
-			$carte = new carteControleur();
-			$carte->carte();
-			break;
-			
-		case "connexion":
-			include_once("connexionControleur.php");
-			$connexion = new connexionControleur;
-			$connexion->connexion();
-			break;
-		
-		case "deconnexion":
-			include_once("deconnexionControleur.php");
-			break;
-		
-		case "inscription":
-			include_once("inscriptionControleur.php");
-			$inscription = new inscriptionControleur();
-			$inscription->inscription();
-			break;
-		
-		case "panier":
-			include_once("panierControleur.php");
-			$panier = new panierControleur();
-			$panier->afficherPanier();
-			break;
-		
-		case "accueil":
-			include_once("vue/accueil.php");
-			break;
-			
-		default:
-			include_once("vue/404.php");
-			break;
-	}	
+
+    # On crée un formulaire invisible pour demander l'appel d'une page
+
+    /*if(isset($_POST["page"])) $page = $_POST["page"];
+    else $page = "accueil";*/
+
+    if(isset($_GET["page"])) $page = $_GET["page"];
+    else $page = "accueil";
+
+    switch($page)
+    {
+        case "carte":
+            include_once("carteControleur.php");
+            $carte = new carteControleur();
+            $carte->carte();
+            break;
+
+        case "connexion":
+            include_once("connexionControleur.php");
+            $connexion = new connexionControleur;
+            $connexion->connexion();
+            break;
+
+        case "deconnexion":
+            include_once("deconnexionControleur.php");
+            break;
+
+        case "inscription":
+            include_once("inscriptionControleur.php");
+            $inscription = new inscriptionControleur();
+            $inscription->inscription();
+            break;
+
+        case "panier":
+            include_once("panierControleur.php");
+            $panier = new panierControleur();
+            $panier->afficherPanier();
+            break;
+
+        case "accueil":
+            include_once("vue/accueil.php");
+            break;
+
+        default:
+            include_once("vue/404.php");
+            break;
+    }	
 ?>
--- reformatage/modele/PanierManager.php
+++ reformatage/modele/PanierManager.php
@@ -3,86 +3,86 @@
 
     class PanierManager extends Model
     {
-		# Constructeur du panier
-		# Fonctionne comme n'importe quel constructeur (mais pas d'appel implicite au constructeur parent)
-		public function __construct()
-		{
-			if(!isset($_SESSION["panier"]))
-			{
-				$_SESSION["panier"] = array();
-			}
-		}
-		
-		public function estVide()
-		{
-			return empty($_SESSION["panier"]);
-		}
-		
-		public function getQteTotale()
-		{
-			$resultat = 0;
-			foreach($_SESSION["panier"] as $numProduit => $produit)
-			{
-				$resultat += $produit["quantite"];
-			}
-			
-			return $resultat;
-		}
-		
-		public function getPrixTotalProduit($numProduit)
-		{
-			return $_SESSION["panier"][$numProduit]["quantite"] * $_SESSION["panier"][$numProduit]["prix"];
-		}
-		
-		public function getPrixPanier()
-		{
-			$resultat = 0;
-			foreach($_SESSION["panier"] as $numProduit => $produit)
-			{
-				$resultat += $this->getPrixTotalProduit($numProduit);
-			}
-			
-			return $resultat;
-		}
-		
-		public function ajouterProduit(array $produit)
-		{
-			if(isset($_SESSION["panier"][$produit[0]]))
-			{
-				$_SESSION["panier"][$produit[0]]["quantite"] += 1;
-			}
-			else
-			{
-				$tmp = array(
-					"libelle" => $produit[1],
-					"description" => $produit[2],
-					"source" => $produit[3],
-					"quantite" => 1,
-					"prix" => $produit[4]
-				);
+        # Constructeur du panier
+        # Fonctionne comme n'importe quel constructeur (mais pas d'appel implicite au constructeur parent)
+        public function __construct()
+        {
+            if(!isset($_SESSION["panier"]))
+            {
+                $_SESSION["panier"] = array();
+            }
+        }
 
-				$_SESSION["panier"][$produit[0]] = $tmp;
-			}
-			
-			header('Location: index.php?page=carte');
-		}
-		
-		public function supprimerProduit(array $produit)
-		{
-			unset($_SESSION["panier"][$produit[0]]);
-		}
-		
-		public function changerQuantiteProduit($libelleProduit, $quantite)
-		{
-			$positionProduit = array_search($libelleProduit,  $_SESSION["panier"]["libelle"]);
-			
-			if($positionProduit != false)
-			{
-				$_SESSION["panier"]["quantite"][$positionProduit] == $quantite;
-			}
-			else
-			{
-				// Erreur ! (à gérer)
-			}
-		}
+        public function estVide()
+        {
+            return empty($_SESSION["panier"]);
+        }
+
+        public function getQteTotale()
+        {
+            $resultat = 0;
+            foreach($_SESSION["panier"] as $produit)
+            {
+                $resultat += $produit["quantite"];
+            }
+
+            return $resultat;
+        }
+
+        public function getPrixTotalProduit($numProduit)
+        {
+            return $_SESSION["panier"][$numProduit]["quantite"] * $_SESSION["panier"][$numProduit]["prix"];
+        }
+
+        public function getPrixPanier()
+        {
+            $resultat = 0;
+            foreach($_SESSION["panier"] as $numProduit => $produit)
+            {
+                $resultat += $this->getPrixTotalProduit($numProduit);
+            }
+
+            return $resultat;
+        }
+
+        public function ajouterProduit(array $produit)
+        {
+            if(isset($_SESSION["panier"][$produit[0]]))
+            {
+                $_SESSION["panier"][$produit[0]]["quantite"] += 1;
+            }
+            else
+            {
+                $tmp = array(
+                    "libelle" => $produit[1],
+                    "description" => $produit[2],
+                    "source" => $produit[3],
+                    "quantite" => 1,
+                    "prix" => $produit[4]
+                );
+
+                $_SESSION["panier"][$produit[0]] = $tmp;
+            }
+
+            header('Location: index.php?page=carte');
+        }
+
+        public function supprimerProduit(array $produit)
+        {
+            unset($_SESSION["panier"][$produit[0]]);
+        }
+
+        public function changerQuantiteProduit($libelleProduit, $quantite)
+        {
+            $positionProduit = array_search($libelleProduit,  $_SESSION["panier"]["libelle"]);
+
+            if($positionProduit != false)
+            {
+                $_SESSION["panier"]["quantite"][$positionProduit] = $quantite;
+            }
+            else
+            {
+                // Erreur ! (à gérer)
+            }
+        }
     }
