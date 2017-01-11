6472b96e252b9d29e7d3e7692b4ab8d9b2272469
Axel <axel.bertrand2@etu.univ-lyon1.fr>
2017-01-11 18:43:50 +0100

    Changement dans la page inscriptions
    J'ai juste changer les champs comme c'était marqué dans le drive

--- MVC/vue/inscription.php
+++ MVC/vue/inscription.php
@@ -16,83 +16,89 @@
 							<?php if(isset($message)) echo "<span class='help-block'>".$message."</span>"; ?>
 						</div>
 						<div class="row">
-							<div class="col-lg-4">
-								<div class="form-group">
-									<label for="pseudo" class="control-label">Pseudo <span class="text-danger">*</span></label>
-									<input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" class="form-control" required/>
+							<div class="col-xs-offset-2 col-xs-8">
+								<div class="row">
+									<div class="col-lg-4">
+										<div class="form-group">
+											<label for="nom" class="control-label">Nom <span class="text-danger">*</span></label>
+											<input type="text" id="nom" name="nom" placeholder="Nom" class="form-control" required/>
+										</div>
+									</div>
+									<div class="col-lg-4">
+										<div class="form-group">
+											<label for="prenom" class="control-label">Prénom <span class="text-danger">*</span></label>
+											<input type="text" id="prenom" name="prenom" placeholder="Prénom" class="form-control" required/>
+										</div>
+									</div>
+									<div class="col-lg-4">
+										<div class="form-group">
+											<label for="pseudo" class="control-label">Pseudo <span class="text-danger">*</span></label>
+											<input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" class="form-control" required/>
+										</div>
+									</div>
 								</div>
-							</div>
-							<div class="col-lg-4">
-								<div class="form-group">
-									<label for="mdp" class="control-label">Mot de passe <span class="text-danger">*</span></label>
-									<input type="password" id="mdp" name="mdp" placeholder="Mot de passe" class="form-control" required/>
+								<div class="row">
+									<div class="col-lg-4">
+										<div class="form-group">
+											<label for="numRue" class="control-label">N°rue</label>
+											<input type="number" id="numRue" name="numRue" placeholder="N°rue" class="form-control"/>
+										</div>
+									</div>
+									<div class="col-lg-8">
+										<div class="form-group">
+											<label for="rue" class="control-label">Rue</label>
+											<input type="text" id="rue" name="rue" placeholder="Nom de rue" class="form-control"/>
+										</div>
+									</div>
 								</div>
-							</div>
-							<div class="col-lg-4">
-								<div class="form-group">
-									<label for="mdpConfirm" class="control-label">Confirmer mot de passe <span class="text-danger">*</span></label>
-									<input type="password" id="mdpConfirm" name="mdpConfirm" placeholder="Confirmer mot de passe" class="form-control" required/>
+								<div class="row">
+									<div class="col-lg-4">
+										<div class="form-group">
+											<label for="codePostal" class="control-label">Code postal</label>
+											<input type="text" id="codePostal" name="codePostal" placeholder="Code postal" class="form-control"/>
+										</div>
+									</div>
+									<div class="col-lg-8">
+										<div class="form-group">
+											<label for="ville" class="control-label">Ville</label>
+											<input type="text" id="ville" name="ville" placeholder="Ville" class="form-control"/>
+										</div>
+									</div>
 								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-6">
-								<div class="form-group">
-									<label for="nom" class="control-label">Nom <span class="text-danger">*</span></label>
-									<input type="text" id="nom" name="nom" placeholder="Nom" class="form-control" required/>
+								<div class="row">
+									<div class="col-lg-8">
+										<div class="form-group">
+											<label for="email" class="control-label">Adresse mail <span class="text-danger">*</span></label>
+											<input type="email" id="email" name="email" placeholder="Adresse e-mail" class="form-control" required/>
+										</div>
+									</div>
+									<div class="col-lg-4">
+										<div class="form-group">
+											<label for="tel" class="control-label">Téléphone <span class="text-danger">*</span></label>
+											<input type="tel" id="tel" name="tel" placeholder="Téléphone" class="form-control" required/>
+										</div>
+									</div>
 								</div>
-							</div>
-							<div class="col-lg-6">
-								<div class="form-group">
-									<label for="prenom" class="control-label">Prénom <span class="text-danger">*</span></label>
-									<input type="text" id="prenom" name="prenom" placeholder="Prénom" class="form-control" required/>
+								<div class="row">
+									<div class="col-lg-6">
+										<div class="form-group">
+											<label for="mdpConfirm" class="control-label">Confirmer mot de passe <span class="text-danger">*</span></label>
+											<input type="password" id="mdpConfirm" name="mdpConfirm" placeholder="Confirmer mot de passe" class="form-control" required/>
+										</div>
+									</div>
+									<div class="col-lg-6">
+										<div class="form-group">
+											<label for="mdp" class="control-label">Mot de passe <span class="text-danger">*</span></label>
+											<input type="password" id="mdp" name="mdp" placeholder="Mot de passe" class="form-control" required/>
+										</div>
+									</div>
 								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-6">
-								<div class="form-group">
-									<label for="email" class="control-label">Adresse mail <span class="text-danger">*</span></label>
-									<input type="email" id="email" name="email" placeholder="Adresse e-mail" class="form-control" required/>
-								</div>
-							</div>
-							<div class="col-lg-6">
-								<div class="form-group">
-									<label for="tel" class="control-label">Téléphone <span class="text-danger">*</span></label>
-									<input type="tel" id="tel" name="tel" placeholder="Téléphone" class="form-control" required/>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-2">
-								<div class="form-group">
-									<label for="numRue" class="control-label">N°rue</label>
-									<input type="number" id="numRue" name="numRue" placeholder="N°rue" class="form-control"/>
-								</div>
-							</div>
-							<div class="col-lg-4">
-								<div class="form-group">
-									<label for="rue" class="control-label">Rue</label>
-									<input type="text" id="rue" name="rue" placeholder="Nom de rue" class="form-control"/>
-								</div>
-							</div>
-							<div class="col-lg-4">
-								<div class="form-group">
-									<label for="ville" class="control-label">Ville</label>
-									<input type="text" id="ville" name="ville" placeholder="Ville" class="form-control"/>
-								</div>
-							</div>
-							<div class="col-lg-2">
-								<div class="form-group">
-									<label for="codePostal" class="control-label">Code postal</label>
-									<input type="text" id="codePostal" name="codePostal" placeholder="Code postal" class="form-control"/>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-offset-8 col-lg-4">
-								<div class="form-group">
-									<button type="submit" class="btn btn-success btn btn-success pull-right">Inscription</button>
+								<div class="row">
+									<div class="col-lg-offset-8 col-lg-4">
+										<div class="form-group">
+											<button type="submit" class="btn btn-success btn btn-success pull-right">Inscription</button>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
