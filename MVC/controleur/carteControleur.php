1092af14ab3e3310640051bb6826fa445f3c943d
Pierre-Elliott Thiboud <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2017-02-14 20:12:05 +0100

    Description de la variable $menus
    Description du contenu de la variable $menus (dans le code et sur le drive)

--- MVC/controleur/carteControleur.php
+++ MVC/controleur/carteControleur.php
@@ -29,16 +29,34 @@
 				//Test pour savoir ce que contiennent les variables
 				//$test[$keyMenu] = array("type" => $typeProduit, "libelle" => $carte[$keyMenu]["libelle"]);
 
+				// On teste le type du produit pour savoir si c'est un menu
 				if(strcmp($typeProduit, "menu") == 0)
 				{
+					// Si le produit est un menu, on l'enlève de la carte
 					unset($carte[$keyMenu]);
+
+					// On récupère les informations du menu (libellé, description, prix)
 					$menus[$keyMenu] = $this->bdd->getInformationsProduit($produit["numProduit"]);
-					$produitCompatibles = $this->bdd->getProduitsCompatibles($produit["numProduit"]);
+
+					// On récupère les numéros des produits compatibles du menu (donc les produits contenus dans le menu)
+					$produitCompatibles = $this->bdd->getProduitsCompatibles($produit["numProduit"]); // C'est un tableau des numProduits2
+
+					// Pour chaque numProduit compatible, on récupère les informations du produit
 					foreach($produitCompatibles as $keyProduit => $produitCompatible)
 					{
 						$menus[$keyMenu]["produits"][$keyProduit] = $this->bdd->getInformationsProduit($produitCompatible["numProduit2"]);
 					}
 				}
+				// La variable $menus est de la forme
+				/*	menus	[$numMenu1]	["libelle"]
+										["description"]
+										["prix"]
+										["produits"]	[$numProduit1]	["libelle"]
+																		["description"]
+														[$numProduit2]	["libelle"]
+																		["description"]
+							[$numMenu2]	(...)
+				*/
 			}
 
 			include_once('vue/carte.php');
