fc74dd18ff70aa478aea3721d27df858df64f5b3
BROUTIER THOMAS <thomas.broutier@gmail.com>
2017-03-01 14:36:26 +0100

    Modification de l'historique commande
    Modification du format de la date

--- MVC/modele/CommandeManager.php
+++ MVC/modele/CommandeManager.php
@@ -77,8 +77,8 @@
         public function getHistoriqueCommande($pseudo)
         {
             $user = $this->um->getNumUser($pseudo);
-            $requete = $this->executerRequete('select date, typeCommande, numCommande from commande
-                                                where numUser= ? order by numCommande desc', array($user));
+            $requete = $this->executerRequete("select DATE_FORMAT(date, '%d-%m-%Y') date, typeCommande, numCommande from commande
+                                                where numUser= ? order by numCommande desc", array($user));
             $resultat = $requete->fetchAll(PDO::FETCH_ASSOC);
 
             foreach ($resultat as $res) {
