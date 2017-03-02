4333eabaf342bec2121683ab2699a9437fb5f870
Pierre-Elliott Thiboud <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2017-03-02 11:55:19 +0100

    Affichage des menus
    Bon c'est un peu moins moche maintenant, mais ça rend pas exactement comme je veux
    Il faut encore :
    - changer l'affichage des produits dans les menus
    - en mettre un nombre fixe par ligne (actuellement c'est pas le cas)
    - modifier l'affichage du prix et du bouton d'ajout (je suis pas satisfait)
    
    Et sinon, la modification que j'avais fait sur la feuille de style de base s'est révélée inutile (voire même merdique, donc je l'ai enlevée)

--- MVC/vue/carte.php
+++ MVC/vue/carte.php
@@ -24,7 +24,7 @@
 				foreach($carte as $produit) {
 					if($i%3 == 0) echo '<div class="row">';
 			?>
-			<div class="col-md-4">
+			<div class="col-lg-4 col-md-4 col-sm-4">
 				<div id="<?php echo $produit["numProduit"]; ?>" class="panel panel-default panel-product">
 					<div class="media img-produit">
 						<a href="#produitModal" data-toggle="modal"
@@ -85,20 +85,31 @@
 			?>
 				<div class="panel panel-default panel-menu">
 					<div class="panel-header">
-						<h2 class="menu-heading text-muted">Menu "<?php echo $menu["libelle"]; ?>"</h2>
-						<p class="text-muted"><?php echo $menu["description"]; ?></p>
-						<p class="text-muted"><?php echo $menu["prix"]; ?> €</p>
+						<div class="row">
+							<h2 class="menu-heading">Menu "<?php echo $menu["libelle"]; ?>"</h2>
+						</div>
+						<div class="row">
+							<div class="col-sm-offset-1 col-lg-7 col-md-7 col-sm-7">
+								<p class="desc-frame text-left"><?php echo $menu["description"]; ?></p>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-4">
+								<p class="text-center">Prix : <?php echo $menu["prix"]; ?> €</p>
+								<button type="button" data-action="ajout" data-produit="1<?php echo $menu["numProduit"]; ?>" class="btn btn-block btn-success btnAjout">
+									<img title='Ajouter au panier' alt='Ajouter au panier' src='images/achat2.png'>
+								</button>
+							</div>
+						</div>
 					</div>
 					<div class="panel-body">
+						<div class="row">
 						<?php
 						$i = 0;
 						foreach($menu["produits"] as $produit)
 						{
-							if($i%3 == 0) echo '<div class="row">';
 						?>
 						<div class="col-md-4">
 							<div class="panel panel-default panel-menu-product">
-                                <a href="#" data-container="body" data-toggle="popover" data-trigger="focus" title="<p class='text-muted'><?php echo $produit["libelle"] ?></p>" data-content="<p class='text-muted'><?php echo $produit["description"] ?></p>" data-placement="auto right" data-html="true">
+                                <a data-container="body" data-toggle="popover" data-trigger="click" title="<p class='text-muted'><?php echo $produit["libelle"] ?></p>" data-content="<p class='text-muted'><?php echo $produit["description"] ?></p>" data-placement="auto right" data-html="true">
 									<div class="panel-heading">
 										<p class="text-muted"><?php echo $produit["libelle"]; ?></p>
 									</div>
@@ -108,20 +119,9 @@
                                 </a>
 							</div>
 						</div>
-
 						<?php
-							$i+=1;
-							if($i%3 == 0) echo '</div>';
 						}
 						?>
-					</div>
-					<div class="panel-footer">
-						<div class="row">
-							<div class="col-lg-offset-9 col-lg-3">
-								<button type="button" data-action="ajout" data-produit="1<?php echo $menu["numProduit"]; ?>" class="btn btn-primary btnAjout">
-									<img title='Ajouter au panier' alt='Ajouter au panier' src='images/achat2.png'>
-								</button>
-							</div>
 						</div>
 					</div>
 				</div>
@@ -130,9 +130,8 @@
 			?>
         </div>
       </div>
-
-		</div>
 	</div>
+</div>
 
 <!-- ======== Fin Code HTML ======== -->
 <?php
